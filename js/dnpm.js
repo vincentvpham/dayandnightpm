1feaaf6904bf0232ec68fe43e7c472e296713ff5
Vincent Pham <vincentvpham@gmail.com>
2016-08-10 22:03:38 -0700

    enable smooth page-scroll; some minor edits

---
+++ js/dnpm.js
@@ -0,0 +1,17 @@
+(function($) {
+
+    // page-scroll (req jQuery Easing plugin)
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+})(jQuery);
